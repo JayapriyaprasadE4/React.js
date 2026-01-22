0c2baa546b1c8e5e05a07d5cb878ebb2843db663
jayapriya <jayapriya>
2026-01-22 17:40:13 +0530

    Todays Practice

---
+++ A.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function A(){
+    return(
+        
+        <div>
+
+        </div>
+    )
+    
+}
+
+export default A
---
+++ One.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function One() {
+    return(
+        <div>
+        <h1>Hello This is React JS</h1>
+        </div>
+    )
+}
+
+export default One
